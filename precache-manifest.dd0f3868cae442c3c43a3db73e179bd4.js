5a8ee0a0035bd3dcc00c3fd327ae20b71ebafd01
Jang <e1668058@cmaisonneuve.qc.ca>
2020-03-04 14:23:38 -0500

    Updates

---
+++ precache-manifest.dd0f3868cae442c3c43a3db73e179bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9400add0c4f12dbdc79f4fc19bc551a9",
+    "url": "/e1668058/my-app/index.html"
+  },
+  {
+    "revision": "d91c93e3a2a81525f154",
+    "url": "/e1668058/my-app/static/css/main.1d9d2b35.chunk.css"
+  },
+  {
+    "revision": "5e5a6e451cc783145bef",
+    "url": "/e1668058/my-app/static/js/2.09ccb745.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/e1668058/my-app/static/js/2.09ccb745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91c93e3a2a81525f154",
+    "url": "/e1668058/my-app/static/js/main.1d6ea105.chunk.js"
+  },
+  {
+    "revision": "8fdc3a4abf3ae3b714fd",
+    "url": "/e1668058/my-app/static/js/runtime-main.9c11de10.js"
+  }
+]);
